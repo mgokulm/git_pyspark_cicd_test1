7b018a7da75b3f15e3a2bc9cfe92ad4d1c0f8025
Gokul Methencheri <mgokulm@gmail.com>
2024-02-11 02:48:51 -0500

    updated Dockerfile

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,4 +1,13 @@
 import pytest
-@pytest.fixture(scope='session')
-def test_add(x,y):
-    assert  add(2,3) == 5
+from jobs.ops import *
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(2,3) == -1
+
+def test_multiply():
+    assert multiply(2,3) == 6
+
+def test_divide():
+    assert divide(12,3) == 4
