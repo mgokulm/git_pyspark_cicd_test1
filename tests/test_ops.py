05fad7ead4f7bd9e83a6c316a3e211dbe0f2523a
Gokul Methencheri <mgokulm@gmail.com>
2024-02-10 21:01:29 -0500

    added pgm ops

---
+++ tests/test_ops.py
@@ -0,0 +1,12 @@
+
+def test_add(x,y):
+    assert  add(2,3) == 5
+
+def test_subtract(x,y):
+    assert  subtract(2,3) == -1
+
+def test_multiply(x,y):
+    assert  multiply(2,3) == 6
+
+def test_divide(x,y):
+    assert  divide(10,5) == 2
